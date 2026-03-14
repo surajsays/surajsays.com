e936ab469ee1519879d202a14c9919dac77444ea
surajsays <surajsays@proton.me>
2026-03-14 14:18:05 +0530

    feat: blog and log listing pages working with static build

---
+++ src/routes/log/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	const posts = import.meta.glob('../../content/log/*.md', { eager: true });
+
+	const allPosts = Object.entries(posts).map(([path, post]) => ({
+		slug: path.split('/').pop().replace('.md', ''),
+		title: post.metadata.title,
+		date: post.metadata.date,
+	})).sort((a, b) => new Date(b.date) - new Date(a.date));
+</script>
+
+<h1>Log</h1>
+
+{#each allPosts as post}
+	<div>
+		<a href="/log/{post.slug}">{post.title}</a>
+		<span>{new Date(post.date).toLocaleDateString('en-IN')}</span>
+	</div>
+{/each}
